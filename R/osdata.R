dd5e0f99435b71d7cf9cd059817e4e17b2f30526
Aleksei Neverov <al_neverov@live.ru>
2016-06-01 18:14:07 +0500

    Some changes

--- R/osdata.R
+++ R/osdata.R
@@ -2,7 +2,13 @@ data("osusers")
 data("osmap")
 
 predict.os <- function(a_Year, a_Month) {
-      return(NULL)
+      osu <- osusers
+      osu$Date <- as.character(osu$Date)
+      osu$Date <- paste(osu$Date,"01",sep="-")
+      osu$Date <- as.Date(osu$Date)
+      fromDate <- osu[nrow(osu),"Date"]
+      toDate <- paste(a_Year,a_Month,"01",sep="-")
+      toDate <- as.Date(toDate)
 }
 
 
